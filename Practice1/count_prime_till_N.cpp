eca5408e154d87ed5840430090c484854706f3dc
kunalsanga <kunalsanga197@gmail.com>
2026-01-30 13:12:32 +0530

    add

---
+++ Practice1/count_prime_till_N.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution{
+    public:
+        bool isprime(int x){
+            if(x<=1) return false;
+            for(int i=2;i*i<=x;i++){
+                if(x%i==0){
+                    return false;
+                }
+            }
+            return true;
+        }
+        int primeuptoN(int n){
+            int count=0;
+            for(int i=2; i<=n;i++){
+                if(isprime(i)){
+                    count++;
+                }
+            }
+            return count;
+        }
+};
+int main(){
+    Solution sol;
+    int n;
+    cout<<"enter n: ";
+    cin>>n;
+    cout<<sol.primeuptoN(n);
+    return 0;
+}
